15d3d527f24ab4af7775ccd08c4e536eec0bcb14
taemin <taemin@taemin-PC>
2016-12-11 13:44:59 +0900

    같은 줄에 수정 있고 새로운 함수를 추가했더랬지

--- GitTest/src/CreateMasterCommitDev/CreaterMasterCommitDe.java
+++ GitTest/src/CreateMasterCommitDev/CreaterMasterCommitDe.java
@@ -5,7 +5,13 @@ public class CreaterMasterCommitDe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// 첨부터 다시 하지
+		// 첨부터 다시 하지 나도 같은 줄에 수정을 하니까 충돌이 나지
+		
+	}
+	
+	// 나도 함수 하나 만들어봤지 리턴값 있찌
+	public int fun(){
+		return 1;
 	}
 
 }
