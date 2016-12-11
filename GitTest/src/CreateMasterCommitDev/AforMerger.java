7a5089404a69ea404bada8c588902e202573d0e5
taemin <taemin@taemin-PC>
2016-12-11 12:45:30 +0900

    마지막 커밋 de

--- GitTest/src/CreateMasterCommitDev/AforMerger.java
+++ GitTest/src/CreateMasterCommitDev/AforMerger.java
@@ -1,5 +1,7 @@
 package CreateMasterCommitDev;
 
 public class AforMerger {
+	
+	// 두 파일 이상 비교
 
 }
--- GitTest/src/CreateMasterCommitDev/CreaterMasterCommitDe.java
+++ GitTest/src/CreateMasterCommitDev/CreaterMasterCommitDe.java
@@ -4,6 +4,8 @@ public class CreaterMasterCommitDe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//  CreaterMasterCommitDe master에서 추가된 소스 코드
 
 	}
 
